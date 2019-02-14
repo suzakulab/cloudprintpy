9e5117ee3837eb88b99bb7f6f93f1a5af7932b9a
theoldmoon0602 <theoldmoon0602@gmail.com>
2019-02-14 15:38:17 +0900

    add dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cloudprintpy",
-    version="0.0.1",
-    install_requires=["requests"],
+    version="0.0.2",
+    install_requires=["requests", "google-auth"],
     packages=['cloudprint']
 )
